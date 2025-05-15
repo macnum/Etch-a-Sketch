226b102b8dd1ff71d2a50fdd29afd14b56e5c461
macnum <w.damilola@yahoo.com>
2025-05-15 13:10:52 +0100

    refactor: move grid creation into a function

--- script/main.js
+++ script/main.js
@@ -8,15 +8,17 @@ const gridWidth = containerWidth / numberOfBoxes;
 const gridHeight = containerHeight / numberOfBoxes;
 
 
+createGrid()
+function createGrid() {
+    for (let i = 1; i <= numberOfBoxes; i++) {
+        for (let j = 1; j <= numberOfBoxes; j++) {
+            const grid = document.createElement("div")
+            grid.style.width = `${gridWidth}px`;
+            grid.style.height = `${gridHeight}px`;
+            grid.classList.add('grid');
+            gridContainer.appendChild(grid);
 
-for (let i = 1; i <= numberOfBoxes; i++) {
-    for (let j = 1; j <= numberOfBoxes; j++) {
-        const grid = document.createElement("div")
-        grid.style.width = `${gridWidth}px`;
-        grid.style.height = `${gridHeight}px`;
-        grid.classList.add('grid');
-        gridContainer.appendChild(grid);
+        }
 
     }
-
 }
