bf09af310cf0170c6496e097276ab7391844dba9
macnum <w.damilola@yahoo.com>
2025-05-15 12:50:29 +0100

    refactor: move grid item width and height into variables

--- script/main.js
+++ script/main.js
@@ -1,12 +1,22 @@
-const gridContainer = document.querySelector('grid-container');
-const containerWidth = 570;
-const containerHeight = 570;
-let numberOfBoxes = 16;
+const gridContainer = document.querySelector('.grid-container');
+
+let numberOfBoxes = 79;
+const containerWidth = 700;
+const containerHeight = 600;
+
+const gridWidth = containerWidth / numberOfBoxes;
+const gridHeight = containerHeight / numberOfBoxes;
+
+
 
 for (let i = 1; i <= numberOfBoxes; i++) {
     for (let j = 1; j <= numberOfBoxes; j++) {
         const grid = document.createElement("div")
-        gridContainer.appendChild(grid)
+        grid.style.width = `${gridWidth}px`;
+        grid.style.height = `${gridHeight}px`;
+        grid.classList.add('grid');
+        gridContainer.appendChild(grid);
+
     }
 
 }
